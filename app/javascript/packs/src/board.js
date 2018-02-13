db549b5871730f8eaa62f58b69a772683ec885a3
Agnes Ching <agnesching19@gmail.com>
2018-02-13 20:22:50 +0000

    addEventListener function on all grids

---
+++ app/javascript/packs/src/board.js
@@ -0,0 +1,53 @@
+const grid_0 = document.getElementById('grid-0');
+grid_0.addEventListener("click", (e) => {
+  e.preventDefault();
+  grid_0.innerHTML = 'X';
+});
+
+const grid_1 = document.getElementById('grid-1');
+grid_1.addEventListener("click", (e) => {
+  e.preventDefault();
+  grid_1.innerHTML = 'X';
+});
+
+const grid_2 = document.getElementById('grid-2');
+grid_2.addEventListener("click", (e) => {
+  e.preventDefault();
+  grid_2.innerHTML = 'X';
+});
+
+const grid_3 = document.getElementById('grid-3');
+grid_3.addEventListener("click", (e) => {
+  e.preventDefault();
+  grid_3.innerHTML = 'X';
+});
+
+const grid_4 = document.getElementById('grid-4');
+grid_4.addEventListener("click", (e) => {
+  e.preventDefault();
+  grid_4.innerHTML = 'X';
+});
+
+const grid_5 = document.getElementById('grid-5');
+grid_5.addEventListener("click", (e) => {
+  e.preventDefault();
+  grid_5.innerHTML = 'X';
+});
+
+const grid_6 = document.getElementById('grid-6');
+grid_6.addEventListener("click", (e) => {
+  e.preventDefault();
+  grid_6.innerHTML = 'X';
+});
+
+const grid_7 = document.getElementById('grid-7');
+grid_7.addEventListener("click", (e) => {
+  e.preventDefault();
+  grid_7.innerHTML = 'X';
+});
+
+const grid_8 = document.getElementById('grid-8');
+grid_8.addEventListener("click", (e) => {
+  e.preventDefault();
+  grid_8.innerHTML = 'X';
+});
