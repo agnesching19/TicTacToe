acfebb6b2bdcdf0764ae1dcdb02d9229e446eb6f
Agnes Ching <agnesching19@gmail.com>
2018-02-13 23:37:09 +0000

    flag X:O on board

--- app/javascript/packs/src/board.js
+++ app/javascript/packs/src/board.js
@@ -14,56 +14,30 @@ initialBoard.addEventListener("click", (e) => {
 });
 
 const grid_0 = document.getElementById('grid-0');
-grid_0.addEventListener("click", (e) => {
-  e.preventDefault();
-  grid_0.innerText = "X";
-});
-
 const grid_1 = document.getElementById('grid-1');
-grid_1.addEventListener("click", (e) => {
-  e.preventDefault();
-  grid_1.innerText = "X"
-});
-
 const grid_2 = document.getElementById('grid-2');
-grid_2.addEventListener("click", (e) => {
-  e.preventDefault();
-  grid_2.innerText = 'X';
-});
-
 const grid_3 = document.getElementById('grid-3');
-grid_3.addEventListener("click", (e) => {
-  e.preventDefault();
-  grid_3.innerText = 'X';
-});
-
 const grid_4 = document.getElementById('grid-4');
-grid_4.addEventListener("click", (e) => {
-  e.preventDefault();
-  grid_4.innerText = 'X';
-});
-
 const grid_5 = document.getElementById('grid-5');
-grid_5.addEventListener("click", (e) => {
-  e.preventDefault();
-  grid_5.innerText = 'X';
-});
-
 const grid_6 = document.getElementById('grid-6');
-grid_6.addEventListener("click", (e) => {
-  e.preventDefault();
-  grid_6.innerText = 'X';
-});
-
 const grid_7 = document.getElementById('grid-7');
-grid_7.addEventListener("click", (e) => {
-  e.preventDefault();
-  grid_7.innerText = 'X';
-});
-
 const grid_8 = document.getElementById('grid-8');
-grid_8.addEventListener("click", (e) => {
-  e.preventDefault();
-  grid_8.innerText = 'X';
-});
 
+let currentPlayer = "X";
+
+function ticTac(){
+  if (this.innerText !== "X" || this.innerText !== "O") {
+    this.innerText = currentPlayer;
+    currentPlayer = currentPlayer == "X" ? "O" : "X";
+  }
+};
+
+document.getElementById("grid-0").onclick = ticTac;
+document.getElementById("grid-1").onclick = ticTac;
+document.getElementById("grid-2").onclick = ticTac;
+document.getElementById("grid-3").onclick = ticTac;
+document.getElementById("grid-4").onclick = ticTac;
+document.getElementById("grid-5").onclick = ticTac;
+document.getElementById("grid-6").onclick = ticTac;
+document.getElementById("grid-7").onclick = ticTac;
+document.getElementById("grid-8").onclick = ticTac;
