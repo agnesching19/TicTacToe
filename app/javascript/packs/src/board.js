375e106cfbf74050c89e30965f4449bb50e3cad5
Agnes Ching <agnesching19@gmail.com>
2018-02-14 00:25:51 +0000

    attempt on showing the winner

--- app/javascript/packs/src/board.js
+++ app/javascript/packs/src/board.js
@@ -41,3 +41,50 @@ document.getElementById("grid-5").onclick = ticTac;
 document.getElementById("grid-6").onclick = ticTac;
 document.getElementById("grid-7").onclick = ticTac;
 document.getElementById("grid-8").onclick = ticTac;
+
+let allThree = function(firstBox, secondBox, thirdBox) {
+  let firstBoxOwner = firstBox.innerText;
+  let secondBoxOwner = secondBox.innerText;
+  let thirdBoxOwner = thirdBox.innerText;
+
+  if ((firstBoxOwner === secondBoxOwner) && (secondBoxOwner === thirdBoxOwner)){
+    if (firstBoxOwner === "X"){
+      alert('X wins!');
+    } else if (firstBoxOwner === "O"){
+      alert('O wins!');
+    }
+  }
+  return null;
+};
+
+let diagonalWinner = function() {
+  let leftDownDiag = allThree(grid_0, grid_4, grid_8);
+  let rightUpDiag = allThree(grid_2, grid_4, grid_6);
+
+  return leftDownDiag || rightUpDiag;
+};
+
+let columnWinner = function() {
+  let leftCol = allThree(grid_0, grid_3, grid_6);
+  let middleCol = allThree(grid_1, grid_4, grid_7);
+  let rightCol = allThree(grid_2, grid_5, grid_8);
+
+  return leftCol || (middleCol || rightCol);
+};
+
+let rowWinner = function() {
+  let topRow = allThree(grid_0, grid_1, grid_2);
+  let middleRow = allThree(grid_3, grid_4, grid_5);
+  let bottomRow = allThree(grid_6, grid_7, grid_8);
+
+  return topRow || (middleRow || bottomRow);
+};
+
+let getWinner = function() {
+  return diagonalWinner() || (rowWinner() || columnWinner());
+};
+
+let winner = getWinner();
+if (winner) {
+  alert("Player " + winner + " won!");
+};
